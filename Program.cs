4876e2ebca95a6ee584ded0a5b62b70c1aec5cfb
renaudesign <renaud.sourmail@gmail.com>
2021-02-18 16:28:23 +0100

    do while c'est mieux

--- Program.cs
+++ Program.cs
@@ -9,35 +9,31 @@ namespace CSHARP_Boucles_Exo4
             //définir une variable aléatoire, l'âge de la dame
             Random aleatoire = new Random(); 
             int secretnumber = aleatoire.Next(1, 51);
-            int tentative = 1;
+            int attempts = 1;
             int success = 0;
 
             Console.WriteLine("Hey mon chou, devine mon âge, petit indice, j'ai entre 1 et 50 ans *wink* *wink*");
                 int input = int.Parse(Console.ReadLine());
 
-            while (success == 0)
+            do
             {
-                
-
                 if (input == secretnumber)
                 {
-                    Console.WriteLine($"Tentative numéro {tentative} \n Bien deviné mon chou *wink* *wink*");
+                    Console.WriteLine($"attempts numéro {attempts} \n Bien deviné mon chou *wink* *wink*");
                     success = 1;}
                 else if (input < secretnumber)
                 {
-                    Console.WriteLine($"Tentative numéro {tentative} \n Vil flatteur, je sais que je ne fait pas mon âge, mais tout de même ! Essaie encore !");
-                    tentative++;
+                    Console.WriteLine($"attempts numéro {attempts} \n Vil flatteur, je sais que je ne fait pas mon âge, mais tout de même ! Essaie encore !");
+                    attempts++;
                     input = int.Parse(Console.ReadLine());
                 }
                 else if (input > secretnumber)
                 {
-                    Console.WriteLine($"Tentative numéro {tentative} \n Ah ! Traites moi tout de suite de vieille peau tant que tu y es! Essaie encore !");
-                    tentative++;
+                    Console.WriteLine($"attempts numéro {attempts} \n Ah ! Traites moi tout de suite de vieille peau tant que tu y es! Essaie encore !");
+                    attempts++;
                     input = int.Parse(Console.ReadLine());
                 }
-
-               
-            }
+            } while (success == 0) ;
             }
         }
     } 
